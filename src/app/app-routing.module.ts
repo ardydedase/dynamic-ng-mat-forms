0d3550392153032698a7aebee416a9602507ade3
ardydedase <ardy.dedase@gmail.com>
2023-02-05 16:05:03 -0600

    1 file modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DynamicFormComponent } from './dynamic-form/dynamic-form.component';
 
-const routes: Routes = [
-  { path: 'dynamic-form', component: DynamicFormComponent },
-];
+const routes: Routes = [{ path: '', component: DynamicFormComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
